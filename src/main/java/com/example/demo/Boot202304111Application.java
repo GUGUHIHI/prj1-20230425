1f61c613e7b4d19791a00a6c6575b92f72824d17
GUGUHIHI <123609789+GUGUHIHI@users.noreply.github.com>
2023-04-26 08:54:17 +0900

    first

---
+++ src/main/java/com/example/demo/Boot202304111Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot202304111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot202304111Application.class, args);
+	}
+
+}
