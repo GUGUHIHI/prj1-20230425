f456265c71123221c9473c87772febb67506d887
GUGUHIHI <123609789+GUGUHIHI@users.noreply.github.com>
2023-04-28 16:52:44 +0900

    20230428

--- src/main/java/com/example/demo/mapper/BoardMapper.java
+++ src/main/java/com/example/demo/mapper/BoardMapper.java
@@ -50,12 +50,30 @@ public interface BoardMapper {
 			""")
 	@Options(useGeneratedKeys = true, keyProperty = "id")
 	int insert(Board board);
-	
 
+	@Select("""
+			<script>
+			<bind name="pattern" value="'%' + search + '%'" />
+			SELECT
+				id,
+				title,
+				writer,
+				inserted
+			FROM Board
+			WHERE 
+				   title  LIKE #{pattern}
+				OR body   LIKE #{pattern}
+				OR writer LIKE #{pattern}
+			ORDER BY id DESC
+			LIMIT #{startIndex}, #{rowPerPage}
+			</script>
+			""")
+	List<Board> selectAllPaging(Integer startIndex, Integer rowPerPage, String search);
+
+	@Select("""
+			SELECT COUNT(*) 
+			FROM Board
+			""")
+	Integer countAll();
 	
 }
-
-
-
-
-
